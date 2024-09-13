8bdfb1315e76df22f0e158c8698fd32d14bb129f
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-09-13 09:42:37 +0530

    Added minor functionality improvements

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 import { appWindow } from '@tauri-apps/api/window';
 await appWindow.setFullscreen(true);
+
+document.getElementById('titlebar-minimize').addEventListener('click', () => appWindow.minimize());
+document.getElementById('titlebar-maximize').addEventListener('click', () => appWindow.toggleMaximize());
+document.getElementById('titlebar-close').addEventListener('click', () => appWindow.close());
