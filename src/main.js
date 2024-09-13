9973f0ff7923c0522a92bebf7628b16c64e3fdd8
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-09-13 08:29:41 +0530

    Added ability to customize resolution

--- src/main.js
+++ src/main.js
@@ -1,7 +1,2 @@
-window.__TAURI_IPC__ = (args) => {
-	//The parent window callback points to the child window
-	window.parent[`_${args.callback}`] = window[`_${args.callback}`];
-
-	// Call the function of the parent window
-	window.parent.__TAURI_IPC__(args);
-};
+import { appWindow } from '@tauri-apps/api/window';
+await appWindow.setFullscreen(true);
